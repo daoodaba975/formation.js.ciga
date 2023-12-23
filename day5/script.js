a0bfbb02aeef1f96f26e761fc62349eb378b40f7
daoodaba975 <davinhoba9@outlook.fr>
2023-12-23 00:06:58 +0000

    🔂 :: upadate

---
+++ day5/script.js
@@ -0,0 +1,31 @@
+//! Récupération des éléments
+let premierDiv = document.getElementById("div1");
+let monH1 = document.querySelector(".titre");
+let deuxiemeDiv = document.getElementById("div2");
+let premierPara = document.querySelector(".para1");
+let deuxiemePara = document.querySelector(".para2");
+let troisiemeDiv = document.getElementById("div3");
+let monImg = document.querySelector(".image0");
+let monNouveauTxt = document.getElementById("monText");
+let monNouveauBtn = document.getElementById("monBouton");
+
+//? Manipulation des éléments
+premierDiv.style.backgroundColor = "aqua";
+monH1.innerHTML = "Nouveau Titre";
+monH1.innerText = "CIGA";
+deuxiemeDiv.addEventListener("mouseover", function () {
+  alert("Test!");
+});
+
+monImg.style.height = "300px";
+monImg.style.width = "300px";
+
+if (monNouveauTxt.length >= 10) {
+  console.log("OK");
+} else {
+  console.log("Non");
+}
+
+monNouveauBtn.addEventListener("click", function () {
+  alert("Ok!");
+});
