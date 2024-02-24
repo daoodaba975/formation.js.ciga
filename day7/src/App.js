d658354cd037c63792c3eadd64139e0ecf4b0db8
daoodaba975 <davinhoba9@outlook.fr>
2024-02-24 21:00:57 +0000

    Update React project.

---
+++ day7/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Test from "./Test";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Mon projet React</p>
+        <Test />
+      </header>
+    </div>
+  );
+}
