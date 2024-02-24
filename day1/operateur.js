d4ba46525648b38a6005b20461600bc9d0b8b1e5
daoodaba975 <davinhoba9@outlook.fr>
2024-02-24 20:03:56 +0000

    Add operateur.js

---
+++ day1/operateur.js
@@ -0,0 +1,35 @@
+// Addition de deux nombres
+let a = 10;
+let b = 5;
+let c = a + b;
+console.log(c);
+
+// Soustraction de deux nombres
+let d = 10;
+let e = 5;
+let f = d - e;
+console.log(f);
+
+// Multiplication de deux nombres
+let g = 10;
+let h = 5;
+let i = g * h;
+console.log(i);
+
+// Division de deux nombres
+let j = 10;
+let k = 5;
+let l = j / k;
+console.log(l);
+
+// Modulo de deux nombres
+let m = 10;
+let n = 5;
+let o = m % n;
+console.log(o);
+
+// Puissance de deux nombres
+let p = 10;
+let q = 5;
+let r = p ** q;
+console.log(r);
