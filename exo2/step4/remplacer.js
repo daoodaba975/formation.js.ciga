fd505c54921b3d379e0e0acd4908655933e49b69
daoodaba975 <davinhoba9@outlook.fr>
2024-04-04 15:45:38 +0000

    Corrected exercise step4.

---
+++ exo2/step4/remplacer.js
@@ -0,0 +1,4 @@
+let chaine = "Bonjour";
+let nouvelleChaine = chaine.replace("Bonjour", "Salut"); // nouvelleChaine vaut "Salut"
+
+console.log(nouvelleChaine);
---
+++ exo2/step4/sous_chaine.js
@@ -0,0 +1,4 @@
+let chaine = "Bonjour";
+let sousChaine = chaine.substring(2, 5); // sousChaine vaut "njo"
+
+console.log(sousChaine);
