e0ec7cf59ab91d2c7f2b20628090f837c05be229
daoodaba975 <davinhoba9@outlook.fr>
2024-04-04 15:45:07 +0000

    Corrected exercise step1.

---
+++ exo2/step1/pair_impair.js
@@ -0,0 +1,6 @@
+function estPair(nombre) {
+  return nombre % 2 === 0;
+}
+
+console.log(estPair(2)); // Affiche true
+console.log(estPair(3)); // Affiche false
---
+++ exo2/step1/rectangle.js
@@ -0,0 +1,8 @@
+function calculerAireRectangle(longueur, largeur) {
+  let aire = longueur * largeur;
+  return aire;
+}
+
+let test = calculerAireRectangle(10, 5);
+
+console.log(test);
