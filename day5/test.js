b5ac08bfcbb1b7c6d7e5cfa0fe86ac432f68958b
daoodaba975 <davinhoba9@outlook.fr>
2024-05-20 21:57:46 +0000

    Update day 5.

--- day5/test.js
+++ day5/test.js
@@ -3,9 +3,9 @@ let bouton = document.getElementById("monBouton");
 let open = document.querySelector(".Op");
 
 // Ajout d'un gestionnaire d'événements
-bouton.addEventListener("mouseover", function () {
-  alert("Le bouton a été survolé!");
-});
+bouton.onclick = function () {
+  alert("Le bouton a été cliqué!");
+};
 
 // Ouvrir le lien du balise a
 open.addEventListener("click", function () {
