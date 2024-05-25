45cb7c758e5b11633db109fc6c7b7d689f85bce4
daoodaba975 <davinhoba9@outlook.fr>
2024-05-25 21:23:56 +0000

    New exercice.

---
+++ exo3/exo1.js
@@ -0,0 +1,19 @@
+// Variables et Opérateurs Arithmétiques
+// Objectif : Calculer la somme, la différence et le produit de trois nombres.
+
+var a = 10;
+var b = -6;
+var c = 100;
+
+// la somme des trois nombres
+var somme = a + b + c;
+
+// la différence des trois nombres
+var diff = a - b - c;
+
+// le produit des trois nombres
+var produit = a * b * c;
+
+console.log("La somme est de : " + somme);
+console.log("La différence est de : " + diff);
+console.log("Le produit est de : " + produit);
