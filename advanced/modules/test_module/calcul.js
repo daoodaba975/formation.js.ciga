f2d2d1d3c855e441157c2fc2ff9280f5fb0ae289
daoodaba975 <davinhoba9@outlook.fr>
2024-08-10 21:50:30 +0000

    [REFACTOR]: Update "modules" directory 🧩

--- advanced/modules/test_module/calcul.js
+++
@@ -1,5 +0,0 @@
-function CalculerMoyenne(n1, n2, n3, n4) {
-  return (n1 + n2 + n3 + n4) / 4;
-}
-
-export var resultat = CalculerMoyenne(12, 16, 9, 19);
--- advanced/modules/test_module/poo.js
+++
@@ -1,5 +0,0 @@
-export function Personne(nom, age, adresse) {
-  this.nom = nom;
-  this.age = age;
-  this.adresse = adresse;
-}
