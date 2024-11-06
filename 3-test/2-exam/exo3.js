0846214b44f10126b06d93a4afc1425a9084fdaa
daoodaba975 <davinhoba9@outlook.fr>
2024-11-06 23:26:42 +0000

    [FEAT]: Add new test exam ⚡

---
+++ 3-test/2-exam/exo3.js
@@ -0,0 +1,5 @@
+function nomComplet(nom, prenom) {
+  return nom + " " + prenom;
+}
+
+console.log("Nom complet : " + nomComplet("Dupont", "Jean"));
---
+++ 3-test/2-exam/exo5.js
@@ -0,0 +1,5 @@
+function somme(nombre1, nombre2, nombre3) {
+  return nombre1 + nombre2 + nombre3;
+}
+
+console.log("La somme est : " + somme(3, 7, 5));
